0f4f115dfeddca3323f671c05a2bf047616bceca
andy78644 <andy690100391@gmail.com>
2022-11-29 16:36:53 +0800

    Finish contract api with retrieving database todo: should choose the data want to respond not all data in database

--- server/app/controllers/contractController.js
+++ server/app/controllers/contractController.js
@@ -2,29 +2,54 @@ const db = require("../models");
 const Contract = db.contracts;
 const Op = db.Sequelize.Op;
 const querystring = require('querystring');
-const getData = require('./getrequest.js');
-
+const getData = require('./getRequest.js');
+//const db = require("../models");
+//const Contract = db.Contract;
 
 apiKey = 'NE2VP5S89SW4TGJSZPCKZHZN2NZ9XKWY4P'
 // Get the Contract with Specific Address
 exports.getByAddress = (req, res) => {
-  const address = req.params.address;
+  const address = req.query.address;
+  //const chainID = req.query.chainID;
   const module = 'account';
   const action = 'balance';
   const tag = 'latest';
-  if (typeof address !== 'string' && address && address.length) {
-    address = address.join(',');
-    action = 'balancemulti';
-  }
-  var query = querystring.stringify({
-    module, action, tag, address, apiKey
-  });
-  var api = getData.getRequest(query);
-  api.then(function(data){
-    console.log(JSON.stringify(data));
-    data = JSON.stringify(data);
-    res.send(data);
-  });
+  //if()
+  Contract.findByPk(address)
+  .then(contract => {
+    if(contract){
+      console.log("found")
+      res.send(contract);
+    } 
+    else {
+      console.log("not found")
+      var query = querystring.stringify({
+        module, action, tag, address, apiKey
+      });
+      var api = getData.getRequest(query);
+      api
+      .then(function(data){
+        const contract = {
+          Address: address,
+          Balance: data["result"],
+        }
+        data = JSON.stringify(data);
+        Contract.create(contract)
+        .then(data => {
+          res.send(data)
+        })
+        .catch(err => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while creating the Tutorial."
+          });
+        })
+        
+      });
+    }
+  })
+  
+  
 }; 
 
 
--- server/app/routes/contractRoutes.js
+++ server/app/routes/contractRoutes.js
@@ -1,15 +1,19 @@
 const contracts = require("../controllers/contractController.js");
 const express = require('express');
 const router = express.Router();
+//const db = require("../models");
+//const user = db.;
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
   next()
 })
 // Find a Contract
+/*
 router.get("/", (req, res) => {
   res.send('Hello from A!')
 });
-router.get("/:address", contracts.getByAddress);
+*/
+router.get("/", contracts.getByAddress);
 //app.use('/api/contracts', router);
 module.exports = router;
 
