a1cadfced175845aa6e4e476742f709befe3ad53
alpha-raylin <raylin910.c@gmail.com>
2023-01-14 16:28:48 +0800

    more info init

--- server/app/controllers/getrequest.js
+++ server/app/controllers/getrequest.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const { resolve } = require('path');
+const util = require('util');
+const EtherscanAPI = require("etherscan-api");
 /**
  * @param {string} chain
  * @returns {string}
@@ -19,45 +22,71 @@ const TESTNET_API_URL_MAP = {
 */
 
 
-module.exports.getRequest = function(query){
+module.exports.getRequest = function(query, address, apiKey){
+    api = EtherscanAPI.init(apiKey);
+
     var client = axios.create({
-        baseURL: 'https://api.polygonscan.com',
+        baseURL: 'https://api.etherscan.io',
         //timeout: timeout
       });
-    return new Promise(function(resolve, reject) {
-        client.get('/api?' + query).then(function(response) {
-          var data = response.data;
-          console.log("echo query: ", '/api?' + query);
-          /*
-          console.log(data);
-          if (data.status && data.status != 1) {
-            let returnMessage = data.message ||'NOTOK';
-            if (data.result && typeof data.result === 'string') {
-              returnMessage = data.result;
-            } else if (data.message && typeof data.message === 'string') {
-              returnMessage = data.message;
-            }
-  
-            //return reject(returnMessage);
-          }
-          console.log('error4');
-          if (data.error) {
-            var message = data.error;
-  
-            if(typeof data.error === 'object' && data.error.message){
-              message = data.error.message;
-            }
-            console.log('error1');
-            return reject(new Error(message));
-          }
-          */
-          //console.log('error3');
-          
+      
+    var ret = {
+      LastTransaction: null,
+      CreateTransaction: null,
+      TokenInfo: null,
+      Balance: null,
+      TransactionCount: null
+    };
+
+    ret.LastTransaction = api.account.txlist(address, 1, 'latest', 1, 1, 'desc');
+    
+    // new Promise(function(resolve, reject) {
+    //     axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=1&sort=desc
+    //     &apikey=${apiKey}`).then(function(response){
+    //       data = response.data;
+    //       console.log(data)
+    //       resolve(data);
+    //     }).catch(function(error) {
+    //       return reject(new Error(error));
+    //     });
+    //   });
+    
+    ret.CreateTransaction = api.account.txlist(address, 1, 'latest', 1, 1, 'asc');
+    
+    // new Promise(function(resolve, reject) {
+    //     axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=1&sort=asc
+    //     &apikey=${apiKey}`).then(function(response){
+    //     data = response.data;
+    //     console.log(data)
+    //     resolve(data);
+    //   }).catch(function(error) {
+    //     return reject(new Error(error));
+    //   });
+    // });
+
+    ret.TokenInfo = new Promise(function(resolve, reject) {
+      axios.get(`https://api.etherscan.io/api?module=token&action=tokeninfo&contractaddress=${address}&apikey=${apiKey}`).then(function(response) {
+          data = response.data;
           resolve(data);
-        }).catch(function(error) {
-          //console.log('error2');
+        }).catch(function(error){
           return reject(new Error(error));
         });
-      });
+    });
+    
+    ret.TransactionCount = api.proxy.eth_getTransactionCount(address, 'latest');
+
+    ret.Balance = api.account.balance(address);
+    // new Promise(function(resolve, reject) {
+    //   axios.get(`https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`).then(function(response) {
+    //     data = response.data;
+    //     resolve(data);
+    //   }).catch(function(error){
+    //     return reject(new Error(error));
+    //   });
+    // });
+
+    return Promise.all([ret.Balance, ret.TokenInfo, ret.LastTransaction, ret.CreateTransaction, ret.TransactionCount]).then( () => {
+      return ret;
+    });
 }
 
