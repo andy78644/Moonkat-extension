db6f922312b6864e2f426bb9bd0336314a4c447b
MilkShakeBro <wyes9830214@gmail.com>
2023-01-17 12:42:45 +0800

    Finish More Prompt.

--- server/app/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const user = require("../controllers/userController.js");
-    var router = require("express").Router();
-
-    // Find a User
-    router.get("/:address", user.getByAddress);
-    
-    app.use('/api/users', router);
-  };
--- web/src/types/contractFeedBackType.ts
+++
@@ -1,5 +0,0 @@
-export default interface contractFeedBack {
-    nameTag: string,
-    categoryTag: string,
-    featureTag: string
-  }
