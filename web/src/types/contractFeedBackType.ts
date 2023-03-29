d58dadddf4e24f6ca7d8f6c617a8aa7eb7e6c019
MilkShakeBro <wyes9830214@gmail.com>
2023-03-29 13:26:45 +0800

    Fix Opensea make collection offer bug.

--- web/src/types/contractFeedBackType.ts
+++
@@ -1,9 +0,0 @@
-// This Interface is implemented to get the API Data
-export default interface contractFeedBack {
-  Provider: string;
-  Address: string | null;
-  Category: string;
-  Name: string;
-  Tag: Array<string>;
-  Description: string;
-}
