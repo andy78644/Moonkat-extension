9b9e9c4a8ca84b75216e43afb8ab39b938d89b6f
Zxiro <zxirocon@gmail.com>
2023-04-06 16:09:20 +0800

    Fix controller and validator

--- server/app/controllers/contractFeedBackController.js
+++ server/app/controllers/contractFeedBackController.js
@@ -14,7 +14,7 @@ exports.reportFeedback = async (req, res) => {
     const report = {
         Provider: req.body.Provider,
         ReportedContract: req.body.Address,
-        CategoryTag: req.body.Category,
+        isMalicious: req.body.isMalicious,
         NameTag: req.body.Name,
         Description: req.body.Description,
         FeatureTagOne: req.body.Tag[0],
@@ -28,7 +28,7 @@ exports.reportFeedback = async (req, res) => {
         const report = {
             "Provider": data.Provider,
             "Address" : data.ReportedContract,
-            "Category" : data.CategoryTag,
+            "isMalicious" : data.isMalicious,
             "Name": data.NameTag,
             "Description": data.Description,
             "Tag":[
@@ -53,7 +53,7 @@ exports.getFeedback = async (req, res) => {
     if(contract){
         const report = {
             "Address" : contract.ReportedContract,
-            "Category" : contract.CategoryTag,
+            "isMalicous" : contract.isMalicous,
             "Name": contract.NameTag,
             "Tag":[
                 contract.FeatureTagOne,
